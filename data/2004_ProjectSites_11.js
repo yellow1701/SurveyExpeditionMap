6f370a7a5e86a9dd1842440c33f8690a7938ab37
yellow1701 <38805829+yellow1701@users.noreply.github.com>
2023-06-09 11:21:11 +0800

    First patch of survey expedition files (June 2023)

---
+++ data/2004_ProjectSites_11.js
@@ -0,0 +1,1 @@
+var json_2004_ProjectSites_11 = {"type":"FeatureCollection","name":"2004_ProjectSites_11","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
---
+++ data/2008_ProjectSites_9.js
@@ -0,0 +1,1 @@
+var json_2008_ProjectSites_9 = {"type":"FeatureCollection","name":"2008_ProjectSites_9","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
